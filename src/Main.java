336463531d592446974b9aa188faef0f17b2b623
Arhadar <irk-art@narod.ru>
2021-03-05 22:37:59 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        int w = 3;
+        while (w > 0) {
+            if (w > 2) {
+                System.out.print("a");
+                w = w - 1;
+                System.out.print("-");
+            }
+            if (w > 1) {
+                System.out.print(" b c");
+
+            }
+
+            if (w == 1) {
+                System.out.print("d");
+
+            }
+
+
+        }
+
+    }
+}
